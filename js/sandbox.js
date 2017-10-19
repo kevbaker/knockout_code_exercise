6b7c601760507f74d5581c7a7388cdb1453d2515
Kevin Baker <kbaker@atomeq.com>
2017-10-19 12:02:04 -0400

    working sandbox with adding objects to list

--- js/sandbox.js
+++ js/sandbox.js
@@ -2,15 +2,32 @@ var vm = {
   heading: ko.observable(),
   store: ko.observable(),
   newListItem: ko.observable(),
-  list: ko.observableArray(), 
-    addListItem: function() {
-        this.list.push(this.newListItem());
-        this.newListItem("");
-    }
+  newBigListItemName: ko.observable(),
+  newBigListItemType: ko.observable(),
+  list: ko.observableArray(),
+  bigList: ko.observableArray(),
+  addListItem: function() {
+    this.list.push(this.newListItem());
+    this.newListItem("");
+  },
+  addBigListItem: function() {
+    let item = {
+        name: this.newBigListItemName(),
+        type: this.newBigListItemType()
+    };
+    this.bigList.push(item);
+  }
 };
 vm.heading("Ninja Heading");
 vm.store("Ninja Store");
+
+
 vm.list.push("one");
 vm.list.push("two");
 vm.list.push("three");
+
+vm.bigList.push({"name": "Kevin", "type":"human"});
+vm.bigList.push({"name": "Robin", "type":"human"});
+vm.bigList.push({"name": "Penny", "type":"baby"});
+
 ko.applyBindings(vm);
