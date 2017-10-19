d2770204c8456d6d24397c23905778089bd2459c
Kevin Baker <kbaker@atomeq.com>
2017-10-19 11:41:13 -0400

    add item to list

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,7 +1,12 @@
 var vm = {
   heading: ko.observable(),
   store: ko.observable(),
-  list: ko.observableArray()
+  newListItem: ko.observable(),
+  list: ko.observableArray(), 
+    addListItem: function() {
+        this.list.push(this.newListItem());
+        this.newListItem("");
+    }
 };
 vm.heading("Ninja Heading");
 vm.store("Ninja Store");
