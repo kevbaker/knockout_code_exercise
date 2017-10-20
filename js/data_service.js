575d985ea362b53e0f8db17ed374d122f34b3ead
Kevin Baker <kbaker@atomeq.com>
2017-10-19 22:15:01 -0400

    working without table edit

--- js/data_service.js
+++ js/data_service.js
@@ -1,125 +1,134 @@
-
+/**
+ * DataService to handle all data for application. Sort of a mock API Client.
+ */
 class DataService {
-    constructor() {
-      this.data = [
-        { Year: 2012, Month: 1, Value: 0.5 },
-        { Year: 2012, Month: 2, Value: 0.5 },
-        { Year: 2012, Month: 3, Value: 0.75 },
-        { Year: 2012, Month: 4, Value: 0.75 },
-        { Year: 2012, Month: 5, Value: 0.5 },
-        { Year: 2012, Month: 6, Value: 0.5 },
-        { Year: 2012, Month: 7, Value: 0.75 },
-        { Year: 2012, Month: 8, Value: 0.75 },
-        { Year: 2012, Month: 9, Value: 0.5 },
-        { Year: 2012, Month: 10, Value: 0.5 },
-        { Year: 2012, Month: 11, Value: 0.75 },
-        { Year: 2012, Month: 12, Value: 0.75 },
-  
-        { Year: 2013, Month: 1, Value: 0.5 },
-        { Year: 2013, Month: 2, Value: 0.5 },
-        { Year: 2013, Month: 3, Value: 0.75 },
-        { Year: 2013, Month: 4, Value: 0.75 },
-        { Year: 2013, Month: 5, Value: 0.5 },
-        { Year: 2013, Month: 6, Value: 0.5 },
-        { Year: 2013, Month: 7, Value: 0.75 },
-        { Year: 2013, Month: 8, Value: 0.75 },
-        { Year: 2013, Month: 9, Value: 0.5 },
-        { Year: 2013, Month: 10, Value: 0.5 },
-        { Year: 2013, Month: 11, Value: 0.75 },
-        { Year: 2013, Month: 12, Value: 0.75 },
-  
-        { Year: 2014, Month: 1, Value: 0.85 }
-      ];
-    }
+  constructor() {
+    this.data = [
+      { Year: 2012, Month: 1, Value: 0.5 },
+      { Year: 2012, Month: 2, Value: 0.5 },
+      { Year: 2012, Month: 3, Value: 0.75 },
+      { Year: 2012, Month: 4, Value: 0.75 },
+      { Year: 2012, Month: 5, Value: 0.5 },
+      { Year: 2012, Month: 6, Value: 0.5 },
+      { Year: 2012, Month: 7, Value: 0.75 },
+      { Year: 2012, Month: 8, Value: 0.75 },
+      { Year: 2012, Month: 9, Value: 0.5 },
+      { Year: 2012, Month: 10, Value: 0.5 },
+      { Year: 2012, Month: 11, Value: 0.75 },
+      { Year: 2012, Month: 12, Value: 0.75 },
 
-    /**
+      { Year: 2013, Month: 1, Value: 0.5 },
+      { Year: 2013, Month: 2, Value: 0.5 },
+      { Year: 2013, Month: 3, Value: 0.75 },
+      { Year: 2013, Month: 4, Value: 0.75 },
+      { Year: 2013, Month: 5, Value: 0.5 },
+      { Year: 2013, Month: 6, Value: 0.5 },
+      { Year: 2013, Month: 7, Value: 0.75 },
+      { Year: 2013, Month: 8, Value: 0.75 },
+      { Year: 2013, Month: 9, Value: 0.5 },
+      { Year: 2013, Month: 10, Value: 0.5 },
+      { Year: 2013, Month: 11, Value: 0.75 },
+      { Year: 2013, Month: 12, Value: 0.75 },
+      { Year: 2014, Month: 1, Value: 0.85 }
+    ];
+  }
+
+  /**
      * Get the data sorted by Year with month objects
      * @return {array} sorted data by year
      */
-    getDataByYear() {
-      let data = this.getData();
-      let sortedData = [];
-  
-      data.forEach(
-        function(item) {
-          sortedData = this._createYear(sortedData, item);
-          let dataYear = this._findYear(sortedData, item);
-          dataYear[item.Month] = item.Value;
-          this._updateTotal(dataYear, item);
-        }.bind(this)
-      );
-      return sortedData;
-    }
+  getDataByYear() {
+    let data = this.getData();
+    let sortedData = [];
 
-    /**
+    data.forEach(
+      function(item) {
+        sortedData = this._createYear(sortedData, item);
+        let dataYear = this._findYear(sortedData, item);
+        dataYear[item.Month] = item.Value;
+        this._updateTotal(dataYear, item);
+      }.bind(this)
+    );
+    return sortedData;
+  }
+
+  /**
      * Find the Year object from the given sortedData 
      * @param {array} sortedData 
      * @param {object} item - item used to match year
      * @return {object} found year item
      */
-    _findYear(sortedData, item) {
-      let foundItem = {};
-      sortedData.forEach(function(dataItem) {
-        if (dataItem.Year == item.Year) {
-          foundItem = dataItem;
-        }
-      });
-      return foundItem;
-    }
+  _findYear(sortedData, item) {
+    let foundItem = {};
+    sortedData.forEach(function(dataItem) {
+      if (dataItem.Year == item.Year) {
+        foundItem = dataItem;
+      }
+    });
+    return foundItem;
+  }
 
-    /**
+  /**
      * Create a Year object for the given item in the sortedData
      * @param {array} sortedData 
      * @param {object} item 
      */
-    _createYear(sortedData, item) {
-      let found = false;
-      sortedData.forEach(function(dataItem) {
-        if (dataItem.Year == item.Year) {
-          found = true;
-        }
-      });
-      if (!found) {
-        let newYear = {
-          Year: item.Year,
-          "1": 0,
-          "2": 0,
-          "3": 0,
-          "4": 0,
-          "5": 0,
-          "6": 0,
-          "7": 0,
-          "8": 0,
-          "9": 0,
-          "10": 0,
-          "11": 0,
-          "12": 0,
-          Total: 0
-        };
-        sortedData.push(newYear);
+  _createYear(sortedData, item) {
+    let found = false;
+    sortedData.forEach(function(dataItem) {
+      if (dataItem.Year == item.Year) {
+        found = true;
       }
-      return sortedData;
+    });
+    if (!found) {
+      let newYear = {
+        Year: item.Year,
+        "1": 0,
+        "2": 0,
+        "3": 0,
+        "4": 0,
+        "5": 0,
+        "6": 0,
+        "7": 0,
+        "8": 0,
+        "9": 0,
+        "10": 0,
+        "11": 0,
+        "12": 0,
+        Total: 0
+      };
+      sortedData.push(newYear);
     }
-    /**
+    return sortedData;
+  }
+  /**
      * Update total for the given year item
      * @param {object} yearItem - year object for setting Total
      * @param {object} item - item object to get value to add to year
      */
-    _updateTotal(yearItem, item) {
-      yearItem.Total += item.Value;
-    }
-    /**
+  _updateTotal(yearItem, item) {
+    yearItem.Total += item.Value;
+  }
+  /**
      * Add an item to the data
      * @param {object} item 
      */
-    addItem(item) {
-      this.data.push(item);
-    }
-    /**
+  addItem(item) {
+    this.data.push(item);
+    this.sortDataByDate();
+  }
+  sortDataByDate() {
+    this.data.sort(function(a, b) {
+      let aa = a.Year * 12 + a.Month;
+      let bb = b.Year * 12 + b.Month;
+      return aa - bb;
+    });
+  }
+  /**
      * Get the data
      * @return {array} data
      */
-    getData() {
-      return this.data;
-    }
+  getData() {
+    return this.data;
   }
+}
--- js/main_view_model.js
+++ js/main_view_model.js
@@ -2,27 +2,58 @@
 class MainViewModel {
     constructor(data) {
       // define properties
-      this.newBigListItemName = ko.observable();
-      this.newBigListItemType = ko.observable();
       this.records = ko.observableArray();
+      this.data = ko.observableArray();
+      this.newYear = ko.observable((new Date()).getFullYear());
+      this.newValue = ko.observable(0.25);
+      this.newMonth = ko.observable(1);
       this.dataSevice = new DataService();
-  
+      this.months = [
+        {text: 'Jan', value: 1},
+        {text: 'Feb', value: 2},
+        {text: 'Mar', value: 3},
+        {text: 'Apr', value: 4},
+        {text: 'May', value: 5},
+        {text: 'Jun', value: 6},
+        {text: 'Jul', value: 7},
+        {text: 'Aug', value: 8},
+        {text: 'Sep', value: 9},
+        {text: 'Oct', value: 10},
+        {text: 'Nov', value: 11},
+        {text: 'Dec', value: 12}
+      ];
+
       // initialize UI
-      this.populateRecords();
+      this.updateRecords();
+      this.updateData();
     }
-    populateRecords() {
-      this.records = this.dataSevice.getDataByYear();
+    updateData() {
+      this.data(this.dataSevice.getData());
+    }
+    updateRecords() {
+      this.records(this.dataSevice.getDataByYear());
+    }
+    _getMonthValue(monthName) {
+      let found = 1;
+      this.months.forEach(function(m){
+        if (m.text == monthName){
+          found = m.value;
+        }
+      });
+      return found;
     }
     addItem() {
       let item = {
-        Year: 2014,
-        Month: 6,
-        Value: 22
+        Year: this.newYear(),
+        Month: this._getMonthValue(this.newMonth()),
+        Value: parseFloat(this.newValue())
       };
-      this.records = ko.observableArray();
-      this.bigList.push(item);
+      this.dataSevice.addItem(item);
+      this.updateData();
+      this.updateRecords();
     }
   }
-  ko.applyBindings(new MainViewModel());
+  let mv = new MainViewModel();
+  ko.applyBindings(mv);
 
   
